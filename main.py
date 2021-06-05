e8edca34489096db355df1cb5f6a5b47e119e162
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2021-06-05 15:42:10 +0700

    Adds main.py
    main.py is the file to execute to run the bot

---
+++ main.py
@@ -0,0 +1,26 @@
+#prefix for bot is "!", you can change it on the PREFIX variable right under the imports
+
+import discord
+
+from discord.ext import commands
+
+TOKEN = "place your token here"
+PREFIX = "!"
+BOT_CHANNEL = 0123123123 # main bot channel id
+
+client = commands.Bot(command_prefix=PREFIX, case_insensitive=True)
+
+@client.event()
+async def on_ready():
+    print("Bot ready")
+    bot_channel = client.get_channel(BOT_CHANNEL)
+    bot_latency = round(client.latency*1000)
+    embed = discord.Embed(title="Bot is ready",
+                          description="Bot is online and ready to be used",
+                          colour=0x218a3d)
+    embed.add_field(name="Latency",
+                    value=f"{bot_latency}ms",
+                    inline=True)
+    bot_channel.send(embed=embed)
+
+client.run(TOKEN)
